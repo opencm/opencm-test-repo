6d708a4b59480ef79b88f29e4da106933cba965a
limor-gs <limor@gigaspaces.com>
2017-03-13 14:59:21 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-release-tool',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/cloudify-cosmo/cloudify-build-system',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
